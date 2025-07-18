da9674bd4f2d0eb7016c367014cca590c0ef0a0c
Evelina Darnostup <evelina.darnostup@gmail.com>
2025-07-18 17:15:52 +0500

    homework4

---
+++ jsBasics/task3.js
@@ -0,0 +1,10 @@
+let ageAlex = 40;
+if(ageAlex>18) {
+console.log(ageAlex)
+};
+console.log(ageAlex); // таке рішення буде вірним?
+
+let ageTom = 30
+let isAdult = ageTom >=18;
+console.log(ageTom);
+console.log(isAdult);
---
+++ jsBasics/task4.js
@@ -0,0 +1,18 @@
+let radius = 20;
+let areaCircle;
+areaCircle = (Math.PI * radius ** 2).toFixed(2);
+console.log(areaCircle);
+
+let lenght = 30;
+let width = 15;
+let areaRectangle;
+areaRectangle = (lenght * width).toFixed(2);
+console.log(areaRectangle);
+
+
+let radius2 = 15;
+let height = 50;
+let areaCircle2;
+areaCircle2 = (Math.PI * radius2 **2 * height).toFixed(2);
+console.log(areaCircle2);
+
