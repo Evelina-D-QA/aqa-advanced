fb7dff2660914a6d672054c4d3535d01a523638b
Evelina Darnostup <evelina.darnostup@gmail.com>
2025-07-22 19:03:07 +0500

    lesson5

---
+++ lesson5-conditionsAndLoops/task1.js
@@ -0,0 +1,14 @@
+let averageGrade = 50;
+if (averageGrade < 60) {
+    console.log ("Unsatisfactory");
+} else if (averageGrade <= 70) {
+    console.log ("Satisfactory");
+} else if (averageGrade <= 80) {
+    console.log ("Good");
+} else if (averageGrade <= 90) {
+    console.log ("Very good");
+} else if (averageGrade <= 100) {
+    console.log ("Excellent");
+} else {
+    console.log ("There is no such assessment");
+}
---
+++ lesson5-conditionsAndLoops/task3.js
@@ -0,0 +1,12 @@
+let number = 3
+for (i = 1; i <= 10; i++) {
+    console.log(`${number} * ${i} = ${number * i}`);
+}
+
+
+let number2 = 3
+let i = 1
+while (i <= 10) {
+    console.log(`${number2} * ${i} = ${number2 * i}`);
+    i++;
+}
